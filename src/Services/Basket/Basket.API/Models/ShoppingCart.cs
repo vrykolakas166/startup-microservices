3f6522789be2ac3ac184b0532fc56ec19f645465
vrykolakas166 <hhtb.bdh@gmail.com>
2025-01-22 20:34:51 +0700

    init basket proj

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.API.Models;
+
+public class ShoppingCart
+{
+    public string Username { get; set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } = [];
+    public decimal TotalPrice => Items.Sum(i => i.Price * i.Quantity);
+    public ShoppingCart()
+    {
+    }
+
+    public ShoppingCart(string username)
+    {
+        Username = username;
+    }
+}
